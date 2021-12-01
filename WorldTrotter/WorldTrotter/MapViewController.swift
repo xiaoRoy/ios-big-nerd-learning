8aa9b6f1cc6f12e477a55ea9bec829514ef8be67
jerli4 <jerli@xogrp.com>
2021-12-01 23:18:45 +0800

    Finished points of interest UI.

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -12,15 +12,18 @@ class MapViewController: UIViewController {
     
     private var mapView: MKMapView!
     
+    private var segmentedControl: UISegmentedControl!
+    
     override func loadView() {
         let tempMapView = MKMapView()
         mapView = tempMapView
         view = tempMapView
         setupMapControl()
+        setupPointsOfInterest()
     }
     
     private func setupMapControl() {
-        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
         segmentedControl.backgroundColor = UIColor.systemBackground
         segmentedControl.selectedSegmentIndex = 0;
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
@@ -34,9 +37,29 @@ class MapViewController: UIViewController {
         let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
         let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
         NSLayoutConstraint.activate([topConstraint, leadingConstraint, trailingConstraint])
-        
-      
     }
+    
+    private func setupPointsOfInterest() {
+        let labelPointOfInterest = UILabel()
+        labelPointOfInterest.text = "Points of Interest"
+        labelPointOfInterest.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(labelPointOfInterest)
+        
+        let switchPointOfInterest = UISwitch()
+        switchPointOfInterest.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(switchPointOfInterest)
+        
+        let switchLeadingConstraint = switchPointOfInterest.leadingAnchor.constraint(equalTo: labelPointOfInterest.trailingAnchor, constant: 16.0)
+        let switchTopConstraint = switchPointOfInterest.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 8.0)
+       
+        
+        let labelLeadingConstraint = labelPointOfInterest.leadingAnchor.constraint(equalTo: segmentedControl.leadingAnchor)
+        let labelTopConstraint = labelPointOfInterest.centerYAnchor.constraint(equalTo: switchPointOfInterest.centerYAnchor)
+        
+        
+        NSLayoutConstraint.activate([labelLeadingConstraint, labelTopConstraint, switchLeadingConstraint, switchTopConstraint])
+    }
+    
     
     @objc
     private func mapTypeChaged(_ mapControl: UISegmentedControl) {
