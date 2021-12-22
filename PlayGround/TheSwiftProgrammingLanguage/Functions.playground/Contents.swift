3c4e103f547e9e4ec0680892ab744ee5015c22d2
jerli4 <jerli@xogrp.com>
2021-12-22 22:34:30 +0800

    Learned function types

--- PlayGround/TheSwiftProgrammingLanguage/Functions.playground/Contents.swift
+++ PlayGround/TheSwiftProgrammingLanguage/Functions.playground/Contents.swift
@@ -74,3 +74,56 @@ func doSwap() {
     print("first is:\(first)")
     print("second is:\(second)")
 }
+
+//Function Types
+func doAddition(first: Int, second: Int) -> Int {
+    first + second
+}
+
+let addition: (Int, Int) -> Int = doAddition
+let anotherAddition: (Int, Int) -> Int = doAddition(first: second:)
+
+func printMathResult(_ calculation: (Int, Int) -> Int, _ first: Int, _ second: Int) {
+    let reuslt = calculation(first, second)
+    print("Result:\(reuslt)")
+}
+
+func setpForward(_ input: Int) -> Int{
+    input + 1
+}
+
+func stepBackward(_ input: Int) -> Int{
+    input - 1
+}
+
+func chooseSetp(isBackward: Bool) -> (Int) -> Int {
+    isBackward ? stepBackward : setpForward(_:)
+}
+
+func demonstrateFunctionTypeAsReturnType() {
+    var currentStep = 3
+    let moveNearerToZero = chooseSetp(isBackward: currentStep > 0)
+    print("Counting to zero:")
+    while currentStep != 0 {
+        print("\(currentStep)...")
+        currentStep = moveNearerToZero(currentStep)
+    }
+    print("zero!")
+    
+}
+
+demonstrateFunctionTypeAsReturnType()
+
+func chooseSetpNested(isBackward: Bool) -> (Int) -> Int {
+    func stepBackward(_ input: Int) -> Int {
+        input - 1
+    }
+    
+    func stepForward(_ input: Int) -> Int {
+        input + 1
+    }
+    
+    return isBackward ? stepBackward(_:) : setpForward(_:)
+    
+}
+
