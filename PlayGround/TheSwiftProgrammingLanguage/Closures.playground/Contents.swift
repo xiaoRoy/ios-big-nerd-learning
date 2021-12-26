f168273c33341da823a23445d88c14bbe1c16b22
jerli4 <jerli@xogrp.com>
2021-12-26 23:49:28 +0800

    Learned closure.

--- PlayGround/TheSwiftProgrammingLanguage/Closures.playground/Contents.swift
+++ PlayGround/TheSwiftProgrammingLanguage/Closures.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Foundation
 
 let names = ["Chris", "Jane", "Ewa", "Alex", "Dean"]
 
@@ -24,3 +25,36 @@ let reversedNamesD = names.sorted(by: { $0 > $1 })
 
 //Operator Methods
 let reversedNamesE = names.sorted(by: >)
+
+//Trailing Closures
+let reversedNamesF = names.sorted() { $0 > $1 }
+
+//Trailing Closures as only one argument
+let reversedNamesG = names.sorted { $0 > $1 }
+
+func transferDigitToName() -> [String]{
+    let digitNames = [
+        0: "Zero", 1: "One", 2: "Two",   3: "Three", 4: "Four",
+        5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"
+    ]
+
+    let numbers = [1, 12, 29, 1201]
+
+    let numbersInNames: [String] = numbers.map {
+        var tempNumber = $0
+        var result = ""
+        repeat {
+            if let digitName = digitNames[tempNumber % 10] {
+                result = digitName + result
+                tempNumber /= 10
+            } else {
+                tempNumber = -1
+            }
+        }while tempNumber > 0
+        return result
+    }
+    return numbersInNames
+}
+
+transferDigitToName()
+
