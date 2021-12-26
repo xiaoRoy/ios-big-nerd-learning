bee4c45e77b157142ee6296338295c2fa2d8fc83
jerli4 <jerli@xogrp.com>
2021-12-26 13:09:01 +0800

    Learned closure expression.

--- PlayGround/TheSwiftProgrammingLanguage/Closures.playground/Contents.swift
+++ PlayGround/TheSwiftProgrammingLanguage/Closures.playground/Contents.swift
@@ -8,7 +8,19 @@ func backward(_ one: String, _ anohter: String) -> Bool {
 
 let reversedNames = names.sorted(by:backward(_:_:))
 
-
+//Closure Expression Syntax
 let reversedNamesA = names.sorted(by: {(one: String, another: String) -> Bool in
-    one > another
+    return one > another
 })
+
+//Inferring Type From Context
+let reversedNamesB = names.sorted(by: {(one, another) in return one > another})
+
+//Implict Returns from Single-Expression Closure
+let reversedNamesC = names.sorted(by: {(one, another ) in one > another})
+
+//Shorthand Argument Names
+let reversedNamesD = names.sorted(by: { $0 > $1 })
+
+//Operator Methods
+let reversedNamesE = names.sorted(by: >)
